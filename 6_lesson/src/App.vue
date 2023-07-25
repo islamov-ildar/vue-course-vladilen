cf91cff1795460d79932a91a0c2e253f0c60f731
islamov-ildar <39472576+islamov-ildar@users.noreply.github.com>
2023-07-25 10:47:47 +0300

    lesson 6 started

---
+++ 6_lesson/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container pt-1">
+    <TheNews v-for="oneNews in news" :key="news" :data="oneNews"/>
+  </div>
+</template>
+
+<script>
+import TheNews from './TheNews.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      news: [
+        'qwe',
+        '12313',
+      ]
+    }
+  },
+  components: {TheNews}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 6_lesson/src/TheNews.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h3>
+    {{ data }}
+    </h3>
+    <button class="btn">Open</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheNews',
+  props: ['data'],
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
