f631f2691a2e1f3949ca5e2413fbfb94380925a8
islamov-ildar <islamov.ildar@gmail.com>
2023-07-26 00:30:40 +0300

    timecode 1:00:42

--- 6_lesson/src/App.vue
+++ 6_lesson/src/App.vue
@@ -1,26 +1,55 @@
 <template>
   <div class="container pt-1">
-    <TheNews v-for="oneNews in news" :key="news" :data="oneNews"/>
+    <div class="card">
+      <h2>ActualNews {{ now }}</h2>
+      <div>Has opened: {{ openRate }}</div>
+      <div>Has readed: {{ readRate }}</div>
+    </div>
+
+    <TheNews
+      v-for="item in news"
+      :key="id"
+      :title="item.title"
+      :id="item.id"
+      :isOpen="item.isOpen"
+      :isRead="item.wasRead"
+      @openNews="openNews"
+      @newsReaded="markAsRead"
+    />
   </div>
 </template>
 
 <script>
-import TheNews from './TheNews.vue';
+import TheNews from "./TheNews.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
+      now: new Date().toLocaleDateString(),
       news: [
-        'qwe',
-        '12313',
-      ]
-    }
+        { title: "PussyDestroyer-1", id: 100, isOpen: false, wasRead: false },
+        { title: "PussyDestroyer-2", id: 200, isOpen: false, wasRead: false },
+      ],
+      openRate: 0,
+      readRate: 0,
+    };
   },
-  components: {TheNews}
-}
+  components: { TheNews },
+  methods: {
+    openNews(data) {
+      console.log(data);
+      this.openRate++;
+    },
+    markAsRead(id) {
+      console.log(id);
+      // this.news = this.news.map()
+      const idx = this.news.findIndex((item) => item.id === id);
+      this.news[idx].wasRead = true;
+      this.readRate++;
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- 6_lesson/src/TheNews.vue
+++ 6_lesson/src/TheNews.vue
@@ -1,20 +1,80 @@
 <template>
   <div class="card">
+    <h2>{{ id }}</h2>
     <h3>
-    {{ data }}
+      {{ title }}
     </h3>
-    <button class="btn">Open</button>
+    <button class="btn" @click="open">
+      {{ isNewsOpen ? "Close" : "Open" }}
+    </button>
+    <div v-if="isNewsOpen">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ex,
+        repellat perspiciatis porro illo nam hic placeat quibusdam nobis omnis
+        soluta perferendis, veritatis, odit voluptate ab maiores at maxime
+        atque!
+      </p>
+      <button v-if="!isRead" class="btn primary" @click="readNews">
+        Read news
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'TheNews',
-  props: ['data'],
-  components: {}
-}
+  name: "TheNews",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      validator(value) {
+        console.log(value);
+        return true;
+      },
+    },
+    id: { type: Number, required: false },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    isRead: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  components: {},
+  emits: {
+    openNews(num) {
+      if (num) {
+        return true;
+      }
+      console.warn("No data in openNews");
+      return false;
+    },
+    newsReaded: null,
+  },
+  data() {
+    return {
+      isNewsOpen: this.isOpen,
+      isNewsRead: this.isRead,
+    };
+  },
+  methods: {
+    open() {
+      this.isNewsOpen = !this.isNewsOpen;
+      if (this.isNewsOpen) this.$emit("openNews", 42);
+    },
+    readNews() {
+      this.isNewsOpen = false;
+      if (!this.isRead) {
+        this.$emit("newsReaded", this.id);
+        // this.isNewsRead = true;
+      }
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
