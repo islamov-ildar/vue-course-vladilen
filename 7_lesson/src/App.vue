b3aef157f29c70e9625701eefd1acd2fa9044361
islamov-ildar <islamov.ildar@gmail.com>
2023-08-17 23:14:24 +0300

    lesson 7 complete

--- 7_lesson/src/App.vue
+++ 7_lesson/src/App.vue
@@ -2,47 +2,55 @@
   <div class="container">
     <form class="card" @submit.prevent="runSubmit">
       <h1>Анкета на Vue разработчика!</h1>
-      <div class="form-control">
-        <label for="name">Как тебя зовут?</label>
-        <input type="text" id="name" placeholder="Введи имя" v-model.trim="name">
-      </div>
+      <AppInput label="Name" placeholder="Input your name" :error="errors.name" v-model="name"/>
+<!--      <div class="form-control">-->
+<!--        <label for="name">Как тебя зовут?</label>-->
+<!--        <input type="text" id="name" placeholder="Введи имя" v-model.trim="name" :class="{invalid: errors.name}">-->
+<!--        <small style="color: red">{{ errors.name }}</small>-->
+<!--      </div>-->
 
       <div class="form-control">
         <label for="age">Выбери возраст</label>
-        <input type="number" id="age" v-model="age">
+        <input type="number" id="age" v-model.number="age">
       </div>
 
       <div class="form-control">
         <label for="city">Твой город</label>
-        <select id="city">
+        <select id="city" v-model="city">
           <option value="spb">Санкт-Петербург</option>
           <option value="msk">Москва</option>
           <option value="kzn">Казань</option>
-          <option selected value="nsk">Новосибирск</option>
+          <option value="nsk">Новосибирск</option>
         </select>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Готов к переезду в Токио?</span>
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Да</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="yes"/> Да</label>
         </div>
 
         <div class="checkbox">
-          <label><input type="radio" name="trip"/> Нет</label>
+          <label><input type="radio" v-model="relocate" name="trip" value="no"/> Нет</label>
         </div>
       </div>
 
       <div class="form-checkbox">
         <span class="label">Что знаешь во Vue?</span>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vuex</label>
+          <label><input type="checkbox" name="skills" v-model="skills" value="vuex" /> Vuex</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue CLI</label>
+          <label><input type="checkbox" name="skills" v-model="skills" value="vue-cli"/> Vue CLI</label>
         </div>
         <div class="checkbox">
-          <label><input type="checkbox"/> Vue Router</label>
+          <label><input type="checkbox" name="skills" v-model="skills" value="vue-router"/> Vue Router</label>
+        </div>
+      </div>
+      <div class="form-checkbox">
+        <span class="label">Company rules</span>
+        <div class="checkbox">
+          <label><input type="checkbox" v-model="agree" /> I Agree</label>
         </div>
       </div>
 
@@ -52,21 +60,46 @@
 </template>
 
 <script>
+import AppInput from "@/AppInput.vue";
   export default {
+    components: { AppInput },
     data(){
       return{
         name: '',
         age: 23,
+        city: 'kzn',
+        relocate: null,
+        skills: [],
+        agree: null,
+        errors: {
+          name: null,
+        }
       }
     },
     methods: {
+      formIsValid() {
+        let isValid = true;
+
+        if(!this.name.length) {
+          isValid = false;
+          this.errors.name = 'Required field'
+        } else {
+          this.errors.name = null
+          isValid = true;
+        }
+
+        return isValid
+      },
       runSubmit() {
-        console.log('runSubmit', this.name, this.age)
+        if(this.formIsValid())
+        console.log('runSubmit', this.name, this.age, this.city, 'To Tokio:', this.relocate, 'Skills:', this.skills, 'Agree:', this.agree)
       }
     }
   }
 </script>
 
-<style>
-
+<style scoped>
+.invalid {
+  border-color: red;
+}
 </style>
---
+++ 7_lesson/src/AppInput.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "AppInput",
+  props: {
+    modelValue: String,
+    error: {
+      type: String,
+    },
+    label: String,
+    placeholder: String,
+  },
+  data(){
+    return {
+      id: 'input-' + Math.random(),
+    }
+  },
+  methods: {
+    change(event){
+      console.log('change');
+      this.$emit('update:modelValue', event.target.value);
+    }
+  },
+  emits: ['update:modelValue']
+}
+</script>
+
+<template>
+  <div class="form-control">
+    <label for="name">{{ label }}</label>
+    <input type="text" :value="modelValue" @input="change" :id="id" :placeholder="placeholder" :class="{invalid: error}">
+    <small style="color: red">{{ error }}</small>
+  </div>
+</template>
+
+<style scoped>
+.invalid {
+  border-color: red;
+}
+</style>
