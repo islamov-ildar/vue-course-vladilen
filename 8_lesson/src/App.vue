3baa5d88b362c910c48e0d5a37915b3979e90d9d
islamov-ildar <islamov.ildar@gmail.com>
2023-10-09 08:44:57 +0300

    lesson 8 has started

---
+++ 8_lesson/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Input Name</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="!name.length">Create Person</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+    }
+  },
+  methods: {
+    createPerson(){
+      console.log('createPerson');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
